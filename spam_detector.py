c078aeff34c57a959b8503f9c36ac10071419285
ankitjana <ankit92005@gmail.com>
2020-04-23 21:53:02 -0400

    fix

--- spam_detector.py
+++ spam_detector.py
@@ -1,45 +1,32 @@
 import os
-from os import listdir
-from os.path import isfile, join
 from os import walk
 import re
-from collections import defaultdict
 
-# getting the current path
+# current directory
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-# getting path of the training files
+# training files
 training_path = os.path.join(dir_path, 'train')
 
 
-'''
-takes in a dictionary of key and vlues (words and frequencies)
-and return dictionary with words in alphabetical order
-'''
-def alphabetical_sort(dictionary):
-    result = {}
-    dictionary.keys()
-    sorted(dictionary.keys())
-    for key in sorted(dictionary.keys()) :
-        result[key] = dictionary[key]
 
-    return result
 
 '''
-prints the output to the file model.txt and optionally to the terminal
-
-p_ham is probability of document being ham document
-p_spam is probability of document being spam document
+p_ham is the probability of document being ham document
+p_spam is the probability of document being spam document
 word_dictionary contains a list of all words
-p_ham is a dictionay with the conditional probablities of all ham words
-p_spam is a dictionay with the conditional probabilites of all spam words
+ham_dict is a dictionary with the conditional probabilities of all ham words
+spam_dict is a dictionary with the conditional probabilities of all spam words
 '''
-def print_result(word_dict, ham_dict, spam_dict, p_ham, p_spam):
-    print_to_console = input("would you like to print file output to the console (0 for no 1 for yes) >")
+
+
+def write_in_file(word_dict, ham_dict, spam_dict, p_ham, p_spam):
+    # print_to_console = input("would you like to print file output to the console (0 for no 1 for yes) >")
     f = open("model.txt", "w+")
     counter = 0
     for word in word_dict:
         counter += 1
+        '''
         if print_to_console == '1':
             print(counter, end="  ")
             print(word, end="  ")
@@ -47,7 +34,7 @@ def print_result(word_dict, ham_dict, spam_dict, p_ham, p_spam):
             print(p_ham[word], end="  ")
             print(spam_dict[word], end="  ")
             print(p_spam[word])
-
+        '''
         f.write(str(counter))
         f.write("  ")
         f.write(str(word))
@@ -61,6 +48,22 @@ def print_result(word_dict, ham_dict, spam_dict, p_ham, p_spam):
         f.write(str(p_spam[word]))
         f.write("\n")
     f.close()
+
+
+'''
+sort the dictionary in alphabetical order
+'''
+
+
+def sort_dictionary(dictionary):
+    result = {}
+    dictionary.keys()
+    sorted(dictionary.keys())
+    for key in sorted(dictionary.keys()) :
+        result[key] = dictionary[key]
+
+    return result
+
 
 
 training_files = []
@@ -81,7 +84,7 @@ for file in training_files:
     if file.find("spam") != -1:
         number_of_spam_documents += 1
 
-# the probabilty of document being ham or spam
+# the probabilty of document being ham or spam [prior probabilty]
 probability_ham = number_of_ham_documents/(number_of_spam_documents+number_of_ham_documents)
 probability_spam = number_of_spam_documents/(number_of_spam_documents+number_of_ham_documents)
 
@@ -97,11 +100,11 @@ for file in training_files:
         for word in words:
             # print(word , end=" - >")
             # print(word)
-
+            #check for blank lines
             if len(word) == 0:
                 continue
 
-
+            #if the word is already in the word dictionary increment the frequency
             if word in word_dictionary:
                 x = word_dictionary[word]
                 x += 1
@@ -109,7 +112,7 @@ for file in training_files:
             else:
                 word_dictionary[word] = 1
 
-            # if the file is a ham file
+            # check if the file is a ham file
             if file.find("ham") != -1:
                 # if the word is already in the ham dictionary increment the frequency
                 if word in ham_word_dictionary:
@@ -118,8 +121,7 @@ for file in training_files:
                     ham_word_dictionary[word] = x
                 else:
                     ham_word_dictionary[word] = 1
-                # make word is also added to spam if it does exist
-                # and with 0 probability
+                # word is also added to spam dictionary if it does not exist with 0 probability
                 if word not in spam_word_dictionary:
                     spam_word_dictionary[word] = 0
 
@@ -133,18 +135,17 @@ for file in training_files:
                 else:
                     spam_word_dictionary[word] = 1
 
-                # make word is also added to ham if it does exist
-                # and with 0 probability
+                # word is also added to ham dictionary if it does not exist with 0 probability
                 if word not in ham_word_dictionary:
                     ham_word_dictionary[word] = 0
 
 
 # count the total frequency of words in both ham and spam files
-total_num_words = 0
+total_words_count = 0
 for i in word_dictionary:
-    total_num_words += word_dictionary[i]
+    total_words_count += word_dictionary[i]
 
-# print("total number of words:", total_num_words)
+# print("total number of words:", total_words_count)
 
 # count the total frequency of ham words only ham files
 total_num_ham_words = 0
@@ -163,28 +164,29 @@ p_ham = {}
 
 # total number of unique words
 number_of_words = len(word_dictionary)
-# and len(word_dictionary) = len(ham_word_dictionary) =  len(spam_word_dictionary)
-# because we inserted every word into the other data category and assigned 0 if it did not exist
 
-# dictionary of ham word probabilites
+
+# dictionary of ham word with conditional probabilites
+# smoothing factor = 0.5
 for i in ham_word_dictionary:
-    p_ham[i] = (ham_word_dictionary[i]+0.5)/(total_num_ham_words+0.5*total_num_words)
+    p_ham[i] = (ham_word_dictionary[i]+0.5)/(total_num_ham_words+0.5*total_words_count)
 
-# dictionary of spam word probabilites
+# dictionary of spam word with conditional probabilites
 for j in spam_word_dictionary:
-    p_spam[j] = (spam_word_dictionary[j]+0.5)/(total_num_spam_words+0.5*total_num_words)
+    p_spam[j] = (spam_word_dictionary[j]+0.5)/(total_num_spam_words+0.5*total_words_count)
 
 # sorts the word dictionary alphabetically
-word_dictionary = alphabetical_sort(word_dictionary)
+word_dictionary = sort_dictionary(word_dictionary)
 
 if __name__ == "__main__":
-    print_result(word_dictionary, ham_word_dictionary, spam_word_dictionary, p_ham, p_spam)
-
-    print("-----------------")
-    print("number_of_spam_documents:", number_of_spam_documents)
-    print("number_of_ham_documents:", number_of_ham_documents)
-    print("P(spam):", probability_spam)
-    print("P(ham):", probability_ham)
-    print("total_num_ham_words:", total_num_ham_words)
-    print('total_num_spam_words:',total_num_spam_words)
-    print("-----------------")
+    write_in_file(word_dictionary, ham_word_dictionary, spam_word_dictionary, p_ham, p_spam)
+    '''
+    print("****************************************************************************")
+    print("Total number of Spam Documents in the training files:", number_of_spam_documents)
+    print("Total number of Ham Documents in the training files:", number_of_ham_documents)
+    print("Probability of a document being a Spam", probability_spam)
+    print("Probability of a document being a Ham", probability_ham)
+    print('Total number of Spam words:', total_num_spam_words)
+    print("Total number of Ham words:", total_num_ham_words)
+    print("****************************************************************************")
+    '''
